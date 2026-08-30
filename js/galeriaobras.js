7f106beea2ae1286d96ba482f737a442e22157fa
bbelu <belensoares@gmail.com>
2026-08-30 20:09:34 -0300

    Update galeriaobras.js

---
+++ js/galeriaobras.js
@@ -0,0 +1,12 @@
+let obras = [
+  {titulo: "Earthly Delights 4.2", anio: "2025", imagen: "img/earthlydelights.jpg"},
+  {titulo: "There's No Distance", anio: "2023", imagen: "img/theresnodistance.jpg"},
+  {titulo: "Still Life (HSB E)", anio: "2023", imagen: "img/stilllife.jpg"},
+  {titulo: "Process 14", anio: "2008", imagen: "img/process14.jpg"},
+  {titulo: "Process 12", anio: "2006", imagen: "img/process12.jpg"}
+];
+
+let obrasDetalle = document.querySelector("#obras")
+let botonSiguiente = document.querySelector("#btnSiguiente");  
+let botonAnterior = document.querySelector ("btnAnterior");
+let indiceObras = 0
