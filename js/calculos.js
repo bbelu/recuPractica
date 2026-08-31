f3dbe9448ab2dd4c340555e9c7a0e9921d7bb3f3
bbelu <belensoares@gmail.com>
2026-08-30 21:39:34 -0300

    let

--- js/calculos.js
+++ js/calculos.js
@@ -1,10 +1,23 @@
-<section id="formInicial">
-  <h2>Datos generales de producción</h2>
-  
-  <label for="???">Cantidad de instalaciones a producir:</label>
-  <input type="number" id="???" name="???">
-  
-  <!-- completá los otros 2 campos -->
-  
-  <button type="button" id="btnIniciar">Comenzar carga de instalaciones</button>
-</section>
+let inputCantidad = document.querySelector("#cantidad");
+let inputHoras = document.querySelector("#horasPordia");
+let inputHonorario = document.querySelector("#honorarioHora");
+let btnIniciar = document.querySelector("#btnIniciar");
+
+
+let inputNombreInst = document.querySelector("#nombreInst");
+let inputCantPersonas = document.querySelector("#cantPersonas");
+let inputCantDias = document.querySelector("#cantDias");
+let btnAgregarInstalacion = document.querySelector("#btnAgregarInstalacion");
+
+let btnCalcular = document.querySelector("#btnCalcular");
+let resulCostDia = document.querySelector("#resulCostDia");
+let maxDiaCost = document.querySelector("#maxDiaCost");
+let porcentCosto = document.querySelector("#porcentCosto");
+let btnReiniciar = document.querySelector("#btnReiniciar");
+
+
+let instalaciones = [];        // array vacío, se va llenando con objetos
+let horasPorDia;                // se asigna en btnIniciar
+let honorarioPorHora;           // se asigna en btnIniciar
+let cantidadInstalaciones;      // se asigna en btnIniciar
+
