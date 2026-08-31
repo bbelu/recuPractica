8b1f7c75567bffa064eb78b0c49e4da8cbdfd638
bbelu <belensoares@gmail.com>
2026-08-30 21:01:37 -0300

    script

---
+++ js/calculos.js
@@ -0,0 +1,10 @@
+<section id="formInicial">
+  <h2>Datos generales de producción</h2>
+  
+  <label for="???">Cantidad de instalaciones a producir:</label>
+  <input type="number" id="???" name="???">
+  
+  <!-- completá los otros 2 campos -->
+  
+  <button type="button" id="btnIniciar">Comenzar carga de instalaciones</button>
+</section>
